084683bdb380652d9bfcb4fad07a72f6a9ca1c49
Olena Penn <olenapenn@gmail.com>
2018-07-03 16:19:40 -1000

    edit edit page

--- public/js/EditRecordForm.js
+++ public/js/EditRecordForm.js
@@ -49,159 +49,103 @@ class EditRecordForm extends React.Component {
         })
     }
 
-    render() {
-        return ( <
-            div className = "row center-align" >
-            <
-            div id = "editForm"
-            className = "col s12 m8 l9" >
-            <
-            h1 className = "center-align" > How are you feeling today ? < /h1> <
-            div className = "row" >
-            <
-            form className = "col s12"
-            onSubmit = {
-                this.handleSubmit
-            } >
-            <
-            div className = "row" >
-            <
-            div className = "input-field col s12" >
-            Mood : < input id = "mood"
-            type = "text"
-            placeholder = "Mood"
-            className = "validate center-align"
-            onChange = {
-                this.handleChange
-            }
-            value = {
-                this.state.mood
-            }
-            / > < /
-            div > <
-            /div>
+    render(){
+        return( 
+            <div className = "row center-align">
+              <div id = "editForm" className = "col s12 m8 l9">
+                <h1 className = "center-align"> How are you feeling today ? </h1> 
+                <div className = "row" >
+                <form className = "col s12"
+                  onSubmit = {this.handleSubmit} >
+                 <div className = "row" >
+                  <div className = "input-field col s12" >
+                   Mood : 
+                   <input id = "mood"
+                     type = "text"
+                     placeholder = "Mood"
+                     className = "validate center-align"
+                     onChange = {this.handleChange}
+                     value = {this.state.mood} /> 
+                  </div> 
+                 </div>
 
-            <
-            div className = "row" >
-            <
-            div className = "input-field col s6" >
-            What did you eat today ? < input id = "food"
-            type = "text"
-            required className = "validate"
-            onChange = {
-                this.handleChange
-            }
-            value = {
-                this.state.food
-            }
-            /> < /
-            div >
+                 <div className = "row" >
+                   <div className = "input-field col s6" >
+                    What did you eat today ? 
+                    <input id = "food"
+                     type = "text"
+                     required className = "validate"
+                     onChange = {this.handleChange}
+                     value = {this.state.food}/> 
+                   </div>
 
-            <
-            div className = "input-field col s6" >
-            What did you want to eat today ? < input id = "foodwant"
-            type = "text"
-            required className = "validate"
-            onChange = {
-                this.handleChange
-            }
-            value = {
-                this.state.foodwant
-            }
-            /> < /
-            div > <
-            /div>
+                   <div className = "input-field col s6" >
+                    What did you want to eat today ? 
+                    <input id = "foodwant"
+                     type = "text"
+                     required className = "validate"
+                     onChange = {this.handleChange}
+                     value = {this.state.foodwant}/> 
+                   </div> 
+                 </div>
 
-            <
-            div className = "row" >
-            <
-            div className = "input-field col s6" >
-            What did you do today ? < input id = "activity"
-            type = "text"
-            required className = "validate"
-            onChange = {
-                this.handleChange
-            }
-            value = {
-                this.state.activity
-            }
-            /> < /
-            div >
+                 <div className = "row" >
+                  <div className = "input-field col s6" >
+                     What did you do today ? 
+                     <input id = "activity"
+                      type = "text"
+                      required className = "validate"
+                      onChange = {this.handleChange}
+                      value = {this.state.activity}/> 
+                  </div >
 
-            <
-            div className = "input-field col s6" >
-            What did you want to do today ? < input id = "activitywant"
-            type = "text"
-            required className = "validate"
-            onChange = {
-                this.handleChange
-            }
-            value = {
-                this.state.activitywant
-            }
-            /> < /
-            div > <
-            /div>
+                  <div className = "input-field col s6" >
+                     What did you want to do today ? 
+                     <input id = "activitywant"
+                      type = "text"
+                      required className = "validate"
+                      onChange = {this.handleChange}
+                      value = {this.state.activitywant}/> 
+                   </div > 
+                 </div>
 
-            <
-            div className = "row" >
-            <
-            div className = "input-field col s6" >
-            What are you grateful
-            for today ? < input id = "grateful"
-            required type = "text"
-            className = "validate"
-            onChange = {
-                this.handleChange
-            }
-            value = {
-                this.state.grateful
-            }
-            /> < /
-            div >
+                 <div className = "row" >
+                  <div className = "input-field col s6" >
+                    What are you grateful for today ? 
+                    <input id = "grateful"
+                     required type = "text"
+                     className = "validate"
+                     onChange = {this.handleChange}
+                     value = {this.state.grateful}/> 
+                  </div >
 
-            <
-            div className = "input-field col s6" >
-            What day is it ? < input id = "date"
-            required type = "text"
-            className = "validate"
-            onChange = {
-                this.handleChange
-            }
-            value = {
-                this.state.date
-            }
-            /> < /
-            div > <
-            /div>
+                  <div className = "input-field col s6" >
+                    What day is it ? 
+                    <input id = "date"
+                     required type = "text"
+                     className = "validate"
+                     onChange = {this.handleChange}
+                     value = {this.state.date}/> 
+                  </div > 
+                 </div>
 
-            <
-            div className = "row" >
-            <
-            div className = "input-field col" >
-            <
-            button className = "left-align btn waves-effect waves-light"
-            type = "submit"
-            name = "action" >
-            <
-            i className = "material-icons right" > < /i>Submit Edits < /
-            button >
-            <
-            a onClick = {
-                () => this.props.toggleState('recordsList', 'editRecord')
-            }
-            className = "waves-effect waves-light btn"
-            id = "submit-edits" >
-            Back to all Records < /a> < /
-            div > < /
-            div > <
-            /form>
-
-            <
-            /
-            div > <
-            /div> < /
-            div >
+                 <div className = "row" >
+                  <div className = "input-field col" >
+                   <button className = "left-align btn waves-effect waves-light"
+                    type = "submit"
+                    name = "action" >
+                    <i className = "material-icons right"></i> Submit Edits </button >
+                    <a onClick = {
+                      () => this.props.toggleState('recordsList', 'editRecord')
+                    }
+                    className = "waves-effect waves-light btn"
+                    id = "submit-edits" > Back to all Records </a> 
+                  </div > 
+                 </div > 
+                </form>
+               </div > 
+              </div> 
+            </div >
         )
     }
 }
